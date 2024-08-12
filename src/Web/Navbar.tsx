f2720e621bb517d07c01a20195ca444253389e80
udeshi <udeshigamage92@gmail.com>
2024-08-12 12:34:56 +0530

    created navbar of portfolio

--- src/Web/Navbar.tsx
+++ src/Web/Navbar.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="top-0 left-0 w-full z-50  bg-slate-900 fixed">
+    <div className="top-0 left-0 w-full z-50   fixed">
       <div className="flex flex-row justify-center items-center">
-        <div className="flex flex-row  position:relative gap-4 p-2 border border-white rounded-full m-2 shadow-md shadow-white">
-          <h1 className="text-white font-bold text-lg">About</h1>
+        <div className="flex flex-row  bg-slate-900 position:relative gap-5 p-2 border border-white rounded-full m-2 shadow-md shadow-white px-10">
+          <h1 className="text-white font-bold text-lg ">About</h1>
           <h1 className="text-white font-bold text-lg">Skills</h1>
           <h1 className="text-white font-bold text-lg">Projects</h1>
           <h1 className="text-white font-bold text-lg">Contact</h1>
